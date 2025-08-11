7bf8d722a7ac4c32bc34ec5ec1ebf08b1aa65cff
PhAr4ujo <phcampos0401@gmail.com>
2025-08-11 11:12:39 -0300

    feat(password-reset): Repository implemented.

---
+++ src/main/java/masterit/masterit/repositories/PasswordResetTokenRepository.java
@@ -0,0 +1,11 @@
+package masterit.masterit.repositories;
+
+import masterit.masterit.entities.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, UUID> {
+    Optional<PasswordResetToken> findByToken(String token);
+}
